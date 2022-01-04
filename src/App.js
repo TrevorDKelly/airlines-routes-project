f12f75d996d8e0782dc487d8d5252a9615176611
Trevor Kelly <Trevor.daniel.kelly@gmail.com>
2022-01-04 12:53:29 -0500

    all data being displayed on the page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
+//import { routes, airlines, airports } from "./data";
+import data from "./data";
+
+const Data = ({type, data}) => {
+  return (
+    <div>
+      <h2>{type}</h2>
+      {data.map(d => {
+        return <p>{Object.keys(d).map(k => <span>{d[k]} </span>)}</p>
+      })}
+    </div>
+  )
+}
 
 const App = () => (
   <div className="app">
-  <header className="header">
-    <h1 className="title">Airline Routes</h1>
-  </header>
-  <section>
-    <p>
-      Welcome to the app!
-    </p>
-  </section>
-</div>
+    <header className="header">
+      <h1 className="title">Airline Routes</h1>
+    </header>
+    <section>
+      <Data type="Routes" data={data.routes} />
+      <Data type="airlines" data={data.airlines} />
+      <Data type="airports" data={data.airports} />
+    </section>
+  </div>
 )
 
 export default App;
