21c3359750ff80417c7406188c3c54b76dd02391
Trevor Kelly <Trevor.daniel.kelly@gmail.com>
2022-01-05 11:20:10 -0500

    next and previous buttons disabled if not an option

--- src/components/page_selection.js
+++ src/components/page_selection.js
@@ -1,24 +1,42 @@
-import React from "react";
-
+import React, { useState } from "react";
 
 const PageSelection = ({ firstIndex, setFirstIndex, total }) => {
+  const [nextDisabled, setNextDisabled] = useState(false);
+  const [previousDisabled, setPreviousDisabled] = useState(true);
+
   function nextPage(e) {
     e.preventDefault();
+    if (previousDisabled) setPreviousDisabled(false)
+    if (firstIndex > total - 51) setNextDisabled(true)
     setFirstIndex(firstIndex + 25);
   }
 
   function previousPage(e) {
     e.preventDefault();
+    if (nextDisabled) setNextDisabled(false)
+    if (firstIndex == 0) setPreviousDisabled(true)
     let newFirst = firstIndex - 25;
     if (newFirst < 0) newFirst = 0;
     setFirstIndex(newFirst);
   }
 
+  function isDisabled(state) {
+    return state ? "disabled" : "";
+  }
+
+  const firstShown = firstIndex + 1;
+  const lastShown = firstShown + 24
+  if (lastShown > total) lastShown = total
+
   return (
     <div className="pagination">
-      <p>Showing {firstIndex + 1}-{firstIndex + 25} of {total}</p>
-      <button onClick={previousPage}>Previous Page</button>
-      <button onClick={nextPage}>Next Page</button>
+      <p>Showing {firstShown}-{lastShown} of {total}</p>
+      <button disabled={previousDisabled} onClick={previousPage}>
+        Previous Page
+      </button>
+      <button disabled={nextDisabled} onClick={nextPage}>
+        Next Page
+      </button>
     </div>
   )
 }
